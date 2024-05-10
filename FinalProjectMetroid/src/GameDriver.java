6402bd024e44e19e8a49abac9d58dc05524e7d56
Aidan Kametz <aidankametz@hotmail.com>
2024-05-10 13:35:44 -0400

    code

--- FinalProjectMetroid/src/GameDriver.java
+++ FinalProjectMetroid/src/GameDriver.java
@@ -5,14 +5,13 @@ public class GameDriver {
 
 	public static void main(String[] args) throws IOException {
 		JFrame frame = new JFrame();
-		frame.setTitle("Metroid Game!");
+		frame.setTitle("Metroid: Nexus");
 		frame.setSize(1600,1000);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);	
-		
-		GameScreen s = new GameScreen();
-		
+		frame.setFocusable(true);
+		GameScreen s = new GameScreen("\\Sprites\\Stage.png");
 		frame.add(s);
 		frame.setVisible(true);
 	}
--- FinalProjectMetroid/src/GameScreen.java
+++ FinalProjectMetroid/src/GameScreen.java
@@ -1,5 +1,26 @@
+import java.awt.Graphics;
+import javax.swing.Timer;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel{
+	private static final long serialVersionUID = 1L;
+	private BufferedImage background;
+	private Timer t = new Timer(17, null);;
+	
+	public GameScreen(String backPath) throws IOException {
+		background = ImageIO.read(new File(backPath));
+		setFocusable(true);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D gn = (Graphics2D)g;
+		gn.drawImage(background, 0, 0, null);
+	}
 
 }
