5f2bec8e1f0419b0620c6aaf4dffcde726aa7a50
Aidan Kametz <aidankametz@hotmail.com>
2024-05-10 13:37:16 -0400

    code

--- TestingGrounds/src/TestingGround.java
+++
@@ -1,9 +0,0 @@
-public class TestingGround {
-	public static void main(String[] args) { 
-		boolean isA = true;
-		for(int i = 1 ; i <= 5 ; i++){
-			for (int j=1; j<=i; j++){
-				if(isA){System.out.print("a");
-				}else{System.out.print("b");
-				}isA = !isA;}
-			System.out.println();}}}
--- TowerOfHanoiying/src/Driver.java
+++
@@ -1,9 +0,0 @@
-
-public class Driver{
-
-	public static void main(String[] args) {
-		Towers t = new Towers();
-		System.out.println(t);
-
-	}
-}
